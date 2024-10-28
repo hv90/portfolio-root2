9e760f2b403d6084795214d925092fe976651fcb
hv90-almeida <hugo.machado@macetado.com>
2024-10-28 04:09:17 -0300

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["/shoelace/dist/assets/icons/list.svg"],
+      external: ["/shoelace/dist/assets/icons/*"],
     },
   },
 });
