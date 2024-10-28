a1211513db7d85f1b1b33de7e4f84cb73c8c73f7
hv90-almeida <hugo.machado@macetado.com>
2024-10-28 03:55:09 -0300

    initial commit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import {
   SlDrawer,
   SlTooltip,
 } from "@shoelace-style/shoelace/dist/react";
-import BurgerIcon from "/shoelace/dist/assets/icons/list.svg";
+import BurgerIcon from "@shoelace-style/shoelace/dist/assets/icons/list.svg";
 import { CSSProperties, useRef } from "react";
 
 const Header: React.FC<{
